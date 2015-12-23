4b8e260a0929b6d1df0d0283d6635dce2c04d11e
Nate Geslin <teamtomkins23@gmail.com>
2015-12-22 18:03:24 -0600

    Scaffolds Rails project * installs rspec * installs shoudla matchers * init npm * init Gulp and tasks * installs guard

---
+++ Gemfile
@@ -0,0 +1,33 @@
+source 'https://rubygems.org'
+ruby '2.2.0'
+
+gem 'rails', '4.2.1'
+gem 'pg'
+gem 'jbuilder', '~> 2.0'
+gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'omniauth', '~> 1.2.2'
+gem 'faker', '~> 1.5.0'
+
+group :development, :test do
+  gem 'rspec-rails', '3.2.1'
+  gem 'capybara', '~> 2.4.4'
+  gem 'byebug'
+  gem 'better_errors', '~>2.1.1'
+  gem 'database_cleaner'
+  gem 'rubocop', require: false
+  gem 'guard-bundler', '2.1.0'
+  gem 'guard-rspec', '4.5.0', require: false
+  gem 'guard-rubocop', '1.2.0', require: false
+  gem 'guard-livereload', '2.5.1', require: false
+  gem 'dotenv-rails', '2.0.2'
+end
+
+group :test do
+  gem 'factory_girl_rails', '4.4.1'
+  gem 'shoulda-matchers', '2.8.0'
+end
+
+group :production do
+    gem 'rails_12factor'
+    gem 'newrelic_rpm', '3.14.0.305'
+end
---
+++ src/scripts/app.js
@@ -0,0 +1,65 @@
+import $ from 'jquery-browserify';
+
+/**
+ * Base view controller
+ *  All other views and controllers are instantiated from this class
+ *
+ * @class App
+ * @author Nate Geslin
+ */
+export default class App {
+    constructor($element) {
+        this.$element = $element;
+
+        return this._init();
+    }
+
+    /**
+     * @method _init
+     * @private
+     * @chainable
+     */
+    _init() {
+
+        return this._createChildren()
+                   ._enable();
+    }
+
+    /**
+     * @method _createChildren
+     * @private
+     * @chainable
+     */
+    _createChildren() {
+        return this;
+    }
+
+    /**
+     * @method _enable
+     * @private
+     * @chainable
+     */
+    _enable() {
+        return this;
+    }
+
+    /**
+     * @method _disable
+     * @private
+     * @chainable
+     */
+    _disable() {
+        return this._destroy();
+    }
+
+    /**
+     * @method _destroy
+     * @private
+     * @chainable
+     */
+    _destroy() {
+        this.$element = null;
+
+        return this;
+    }
+}
