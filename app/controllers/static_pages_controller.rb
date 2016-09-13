9bbb5569ee8becf19c7770a84d178c00cf9745e3
Nate Geslin <teamtomkins23@gmail.com>
2016-09-12 19:57:56 -0500

    Adds new methods to static_pages * Adds new pages * Tweaks styles for mainnav

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def bookkeeping_in_the_cloud
+  end
+
+  def about
+  end
+
+  def hiring
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  get 'bookkeeping-in-the-cloud', to: 'static_pages#bookkeeping_in_the_cloud' #, as: :bookkeeping_in_the_cloud
+  get 'about', to: 'static_pages#about'
+  get 'hiring', to: 'static_pages#hiring'
 end
